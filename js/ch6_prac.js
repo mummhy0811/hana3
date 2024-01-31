48ab74b6a374c6892da2fec8fc235ee6e61172bf
mummhy0811 <mummhy0811@gmail.com>
2024-01-31 10:33:10 +0900

    Refac: clean up func once

--- js/ch6_prac.js
+++ js/ch6_prac.js
@@ -8,25 +8,22 @@ console.log(fn(3, 8)); // undefined
 
 */
 
-function once(f){
+function once(f) {
   let didRun = false;
-  return function (...args){
-    if(!didRun) {
-      didRun=true;
-      //return f.call(this, ...args);
-      return f.apply(this, args);
-    }
-    return;
+  return function (...args) {
+    if (didRun) return;
+    didRun = true;
+    return f.apply(this, args);
+    // return (didRun=true),  f.apply(this, args); //동일 코드
   };
 }
 
-function f( x, y){
+function f(x, y) {
   return `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!  ${this.id}`;
-} 
+}
 
 const thisObj1 = { id: 100 };
 const thisObj2 = { id: 200 };
 const fn = once(f);
-console.log(fn.call(thisObj1, 1, 6)); 
-console.log(fn(1,6));
-
+console.log(fn.call(thisObj1, 1, 6));
+console.log(fn(1, 6));
