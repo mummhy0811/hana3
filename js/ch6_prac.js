b3eada7a057392012bb68cece5ac5cfdbff65197
mummhy0811 <mummhy0811@gmail.com>
2024-01-31 10:18:06 +0900

    Add: ch6 prac 문제 추가

--- js/ch6_prac.js
+++ js/ch6_prac.js
@@ -1,3 +1,13 @@
+/*
+함수를 한번만 실행하게 하는 once 함수를 작성하시오.
+ex)
+const fn = once((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
+console.log(fn(1, 6)); // 금일 운행금지 차량은 끝번호 1, 6입니다!
+console.log(fn(2, 7)); // undefined
+console.log(fn(3, 8)); // undefined
+
+*/
+
 function once(f){
   let didRun = false;
   return function (...args){
