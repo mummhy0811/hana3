036a6dd641267129703b9960cf816a2d59d6c6a7
mummhy0811 <mummhy0811@gmail.com>
2024-02-01 17:46:05 +0900

    Refac: change ch10 prac loop

--- js/ch10_prac.js
+++ js/ch10_prac.js
@@ -7,14 +7,11 @@ const kim = {id: 2, name: 'Kim', dept: 2};
 const emps = [ hong, kim, {id:3, name: 'Park', dept: 2}, {id: 4, name: 'Choi', dept: 2} ];
 
 const deptMap = new Map();
-for(let l of depts){
-    deptMap.set(l.id, l);
-}
+depts.forEach((team)=>deptMap.set(team.id, team));
 
 const empMap = new Map();
-for(let l of emps){
-    empMap.set(l.id, l);
-}
+emps.forEach((emp)=>empMap.set(emp.id, emp));
+
 const empDept = new Map();
 for(let emp of empMap.values()){
     empDept.set(emp,deptMap.get(emp.dept))
@@ -25,5 +22,4 @@ console.log(empMap); // Map(2) { 1 => {id: 1, name: 'Hong', dept: 1}, 2 => {id: 
 console.log(empDept); // Map(4) { { id: 1, name: 'Hong' } => { id: 1, dname: '인사팀' }, { id: 2, name: 'Kim' } => { id: 2, dname: '개발팀' }, { id: 3, name: 'Park' } => { id: 2, dname: '개발팀' }, { id: 4, name: 'Choi' } => { id: 2, dname: '개발팀' } }
 console.log(empDept.get(kim).dname); // '개발팀'
 // 개발팀 직원 목록 출력 ⇒ Kim, Park, Choi
-
 
