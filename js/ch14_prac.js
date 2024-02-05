a607a9d11c1c6a6ba215ac3d3e04696a2d126b43
mummhy0811 <mummhy0811@gmail.com>
2024-02-05 17:13:48 +0900

    Feat: solve ch14 prac(promise)

---
+++ js/ch14_prac.js
@@ -0,0 +1,46 @@
+// setTimeout(function () {
+//   console.log("depth1", new Date());
+//   setTimeout(function () {
+//     console.log("depth2", new Date());
+//     setTimeout(function () {
+//       console.log("depth3", new Date());
+//       throw new Error("Already 3-depth!!");
+//     }, 3000);
+//   }, 2000);
+// }, 1000);
+
+// console.log("START!", new Date());
+
+
+
+const depthTimer = n=> new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        if(n<=2) resolve(console.log(`depth${n}`, new Date()));
+        if(n>=3) {
+            console.log(`depth${n}`, new Date())
+            reject(new Error("Already 3-depth!!"))
+        };
+    },n*1000);
+    
+});
+
+depthTimer(1)
+.then(()=>depthTimer(2)) 
+.then(()=>depthTimer(3))
+.catch(err => {console.error(err)});
+
+
+//강사님 코드
+const depthTimer2 = depth=> new Promise((resolve, reject)=>{
+
+    setTimeout(()=>{
+        console.log(`depth${depth}!!`, new Date());
+        if(depth>=3) reject( new Error("Already 3-depth!!"));
+        resolve(depth+1);
+    },depth*1000);
+});
+
+depthTimer2(1)
+.then(depthTimer2) 
+.then(depthTimer2)
+.catch(err => console.error(err));
