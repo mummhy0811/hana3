da65713b4a3786ce1d46cc8028c61f4ef9aec690
mummhy0811 <mummhy0811@gmail.com>
2024-02-19 09:04:38 +0900

    init React

---
+++ rbvite/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+
+  plugins: ['react-refresh', 'jsx-a11y', 'prettier'],
+
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'react/no-array-index-key': 'error',
+  },
+};
