c13a1a269622d3156b1cfee3efe7658e8fbdfb16
mummhy0811 <mummhy0811@gmail.com>
2024-02-19 17:13:47 +0900

    Fix: change useState to useRef

--- react/rbvite/src/components/Login.tsx
+++ react/rbvite/src/components/Login.tsx
@@ -1,24 +1,46 @@
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 type Props = {
   login: (id:number, name:string) =>void;
 }
 export const Login = ({ login }: Props) => {
-  const [id, setId] = useState(0);
-  const [name, setName] = useState(' ');
+  // const [id, setId] = useState(0);
+  //const [name, setName] = useState(' ');
+  const idRef=useRef<HTMLInputElement>(null);
+  const nameRef=useRef<HTMLInputElement>(null);
+
+  const makeLogin=(e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault(); //submit을 무력화
+    if(!idRef.current?.value){
+      alert('User ID를 입력하세요');
+      idRef.current?.focus();//id 입력창으로 포커스 이동
+      return;
+    }
+    
+    if(!nameRef.current?.value){
+      alert('User Name을 입력하세요');
+      nameRef.current?.focus();//name 입력창으로 포커스 이동
+      return;
+    }
+    const id=idRef.current.value;
+    const name=nameRef.current.value;
+    login(+id, name)
+  }
 
   return (
     <>
-      <form>
+      <form onSubmit={makeLogin}>
         <div>
           LoginID:
-          <input type='text' onChange={(e) => setId(+e.currentTarget.value)} />
+          {/* <input type='text' onChange={(e) => setId(+e.currentTarget.value)} /> */}
+          <input type='number' ref={idRef} />
         </div>
         <div>
           LoginName:
-          <input type='text' onChange={(e) => setName(e.currentTarget.value)}/>
+          {/* <input type='text' onChange={(e) => setName(e.currentTarget.value)}/> */}
+          <input type='text' ref={nameRef} />
         </div>
-        <button onClick={() => login(id, name)}>Sign-in</button>
+        <button type='submit'>Sign-in</button>
       </form>
     </>
   );
